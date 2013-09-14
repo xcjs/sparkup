cb39ecb9dd87dccef09c8400370a54bd7c4f0c26
Zackary Lowery <zlowery@xcjs.com>
2013-09-13 20:04:54 -0400

    Removed the loop from the test scratchpad. This was used to check for any signs memory leaks over time with parent references.

--- tests/test.php
+++ tests/test.php
@@ -5,33 +5,31 @@
 	use XCJS\Sparkup\TextNode as TextNode;
 	use XCJS\Sparkup\BaseElement as BaseElement;
 
-	while(true) {
-		$tn = new TextNode('I am a TextNode.');
-		$p = new BaseElement('p', array('class' => 'pretty', 'id' => 'important'));
-		$sp = new BaseElement('span');
+	$tn = new TextNode('I am a TextNode.');
+	$p = new BaseElement('p', array('class' => 'pretty', 'id' => 'important'));
+	$sp = new BaseElement('span');
 
-		$sp->addChild($tn);
-		$p->addChild($sp);
+	$sp->addChild($tn);
+	$p->addChild($sp);
 
-		echo $p->write();
+	echo $p->write();
 
-		$p->removeChild($sp);
+	$p->removeChild($sp);
 
-		echo $p->write();
+	echo $p->write();
 
-		$sp->removeChild($tn);
-		$p->addChild($sp);
+	$sp->removeChild($tn);
+	$p->addChild($sp);
 
-		echo $p->write();
+	echo $p->write();
 
-		$p->removeChild(0);
-		$sp->addChild($tn);	
-		$p->addChild($sp);
+	$p->removeChild(0);
+	$sp->addChild($tn);	
+	$p->addChild($sp);
 
-		echo $p->write();
+	echo $p->write();
 
-		unset($tn);
-		unset($p);
-		unset($sp);
-	}
+	unset($tn);
+	unset($p);
+	unset($sp);
 ?>
